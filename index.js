80ad26c0df66bec36205687e2141bb4cccd96837
lyndoncortez <lyndoncortez@yahoo.com>
2024-01-06 03:03:33 +0800

    add ESlint

--- index.js
+++ index.js
@@ -1,107 +1,106 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const Person = require('./models/person')
+const express = require('express');
+const app = express();
+const Person = require('./models/person');
 
-var morgan = require('morgan')
+var morgan = require('morgan');
 
-app.use(express.json())
-app.use(express.static('dist'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.json());
+app.use(express.static('dist'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
-morgan.token('data', (req, res) => {
-    return JSON.stringify(req.body);
+morgan.token('data', (req) => {
+  return JSON.stringify(req.body);
 });
 
 // const generateId = () => {
 //     const maxId = contacts.length > 0
 //         ? Math.max(...contacts.map(c => c.id))
 //         : 0
-    
 //         return maxId + 1
 // }
 
 app.get('/info', (request, response) => {
   Person.find({}).then(contacts => {
-    const currentDate = new Date().toString()
-    response.send(`<p>Phonebook has info for ${contacts.length} people <br><br>${currentDate} </p>`)
-  })
-})
+    const currentDate = new Date().toString();
+    response.send(`<p>Phonebook has info for ${contacts.length} people <br><br>${currentDate} </p>`);
+  });
+});
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(contacts => {
-    response.json(contacts)
-  })
-})
+    response.json(contacts);
+  });
+});
 
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(contact => {
-      response.json(contact)
-    })
-})
+  Person.findById(request.params.id).then(contact => {
+    response.json(contact);
+  });
+});
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body
+  const { name, number } = request.body;
 
   Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
     .then(updatedContact => {
-      response.json(updatedContact)
+      response.json(updatedContact);
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body;
 
-    const contact = new Person({
-      name: body.name,
-      number: body.number
+  const contact = new Person({
+    name: body.name,
+    number: body.number
+  });
+
+  contact.save()
+    .then(savedContact => {
+      response.json(savedContact);
     })
+    .catch(error => next(error));
+  // let errorJson = {error: ''}
 
-    contact.save()
-      .then(savedContact => {
-        response.json(savedContact)
-      })
-      .catch(error => next(error))
-    // let errorJson = {error: ''}
+  // if(!body.name || !body.number || existingContact) {
+  //     if (!body.name) {
+  //         errorJson = { error: 'name is missing' };
+  //       } else if (!body.number) {
+  //         errorJson = { error: 'number is missing' };
+  //       } else if (existingContact) {
+  //         errorJson = { error: 'name must be unique' };
+  //       }
 
-    // if(!body.name || !body.number || existingContact) {
-    //     if (!body.name) {
-    //         errorJson = { error: 'name is missing' };
-    //       } else if (!body.number) {
-    //         errorJson = { error: 'number is missing' };
-    //       } else if (existingContact) {
-    //         errorJson = { error: 'name must be unique' };
-    //       }
-
-    //     return response.status(400).json(errorJson)
-    // }
-})
+  //     return response.status(400).json(errorJson)
+  // }
+});
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
+    .then(() => {
+      response.status(204).end();
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/person.js
+++ models/person.js
@@ -1,44 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then(() => {
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const contactSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: [3, 'minimum of 3 characters']
-    },
-    number: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function (value) {
-          // Custom phone number validation
-          const phoneNumberRegex = /^[0-9]{2,3}-[0-9]+$/;
-          return phoneNumberRegex.test(value);
-        },
-        message: 'Invalid phone number format. Use the format XX-XXXXXXX or XXX-XXXXXXX.',
+  name: {
+    type: String,
+    minLength: [3, 'minimum of 3 characters']
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Custom phone number validation
+        const phoneNumberRegex = /^[0-9]{2,3}-[0-9]+$/;
+        return phoneNumberRegex.test(value);
       },
-    }
-})
+      message: 'Invalid phone number format. Use the format XX-XXXXXXX or XXX-XXXXXXX.',
+    },
+  }
+});
 
 contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-module.exports = mongoose.model('Person', contactSchema)
+module.exports = mongoose.model('Person', contactSchema);
